226fc3f1a12e3c1622f8129b07028c8c43accbe9
Iron.Snowflake <jadrubio@gmail.com>
2021-06-30 22:27:51 -0400

    added modal scrollbar and stylings

--- src/components/Cards/InfoModal/InfoModal.js
+++ src/components/Cards/InfoModal/InfoModal.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Modal, ModalBody } from "react-bootstrap";
+import { Modal } from "react-bootstrap";
 import InfoTable from '../../Tables/InfoTable';
 import ConditionsTab from '../../Conditions/ConditionsTabs';
+import './InfoModal.css';
 
 function InfoModal({ info, setShowInfoModal, showInfoModal }) {
   return (
@@ -10,24 +11,27 @@ function InfoModal({ info, setShowInfoModal, showInfoModal }) {
       show={showInfoModal}
       onHide={() => setShowInfoModal(false)}
       aria-labelledby="example-modal-sizes-title-lg"
+      scrollable
     >
       <Modal.Header closeButton>
         <Modal.Title id="example-modal-sizes-title-lg">
           {info.title}
         </Modal.Title>
       </Modal.Header>
-      <Modal.Body>{info.shortDescription && info.shortDescription}</Modal.Body>
-      {info.longerDescription && info.longerDescription.map(descrip => <Modal.Body>{descrip}</Modal.Body>)}
-      {info.table && (
-        <Modal.Body>
-          <InfoTable tableInfo={info.table} />
-        </Modal.Body>
-      )}
-      {info.conditions.length > 0 &&
-        <ModalBody>
-          <ConditionsTab cardConditions={info.conditions} />
-        </ModalBody>
-      }
+      <Modal.Body>
+        <div>{info.shortDescription && info.shortDescription}</div>
+        {info.longerDescription && info.longerDescription.map(descrip => <div>{descrip}</div>)}
+        {info.table && (
+          <div>
+            <InfoTable tableInfo={info.table} />
+          </div>
+        )}
+        {info.conditions.length > 0 &&
+          <div>
+            <ConditionsTab cardConditions={info.conditions} />
+          </div>
+        }
+      </Modal.Body>
       {info.di && (<Modal.Footer className="d-inline"><b>Di Roll: </b>{info.di}</Modal.Footer>)}
     </Modal>
   );
